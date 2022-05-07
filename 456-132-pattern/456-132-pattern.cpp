311ffecc10fd9269f78002717a5265dac9d0f237
Ahmed Saed <37080003+Ahmedsaed@users.noreply.github.com>
2022-05-07 22:12:52 +0200

    Time: 108 ms (38.00%), Space: 38.3 MB (69.05%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        if (nums.size() < 3) {
-            return false;
-        }
-        
-        vector<int> min_array(nums.size());
-        min_array[0] = nums[0];
-
-        for (int i = 1; i < nums.size(); i++) {
-            min_array[i] = min(min_array[i - 1], nums[i]);
-        }
-
-        for (int j = nums.size() - 1, k = nums.size(); j > 0; j--) {
-            if (nums[j] <= min_array[j]) {
-                continue;
+        int s3 = INT_MIN;
+        stack<int> st;
+        for(int i = nums.size() - 1; i >= 0; i --) {
+            if(nums[i] < s3) return true;
+            else while( !st.empty() && nums[i] > st.top() ){ 
+              s3 = st.top(); st.pop(); 
             }
-            while (k < nums.size() and nums[k] <= min_array[j]) {
-                k++;
-            }
-            if (k < nums.size() and nums[k] < nums[j]) {
-                return true;
-            }
-            nums[--k] = nums[j];
+            st.push(nums[i]);
         }
         return false;
     }
